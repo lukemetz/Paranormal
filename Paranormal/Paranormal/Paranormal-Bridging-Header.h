bf02444329bd52bb04470dd4b45847ee65985cfa
Luke Metz <luke.metz@students.olin.edu>
2014-10-11 16:58:56 -0400

    Cocos2d now inside project.

---
+++ Paranormal/Paranormal/Paranormal-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Paranormal-Bridging-Header.h
+//  Paranormal
+//
+//  Created by Luke Scope on 10/11/14.
+//  Copyright (c) 2014 spritebuilder. All rights reserved.
+//
+
+#ifndef Paranormal_Paranormal_Bridging_Header_h
+#define Paranormal_Paranormal_Bridging_Header_h
+
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+#import <Foundation/Foundation.h>
+
+#endif
