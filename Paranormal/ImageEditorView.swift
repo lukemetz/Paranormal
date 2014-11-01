d028a6904f58ae0ab2516ec9876336fb67034398
Diana Vermilya <dianavermilya@gmail.com>
2014-11-01 15:24:26 -0400

    image editor : delegate mouse events from temp view to controller

---
+++ Paranormal/ImageEditorView.swift
@@ -0,0 +1,29 @@
+//
+//  ImageEditorView.swift
+//  Paranormal
+//
+//  Created by Diana Vermilya on 11/1/14.
+//  Copyright (c) 2014 spritebuilder. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+import AppKit
+
+
+class ImageEditorView : NSImageView {
+    
+    @IBOutlet var delegate : NSResponder?
+    
+    override func mouseDown(theEvent: NSEvent) {
+        delegate?.mouseDown(theEvent)
+    }
+    
+    override func mouseDragged(theEvent: NSEvent) {
+        delegate?.mouseDragged(theEvent)
+    }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        delegate?.mouseUp(theEvent)
+    }
+}
