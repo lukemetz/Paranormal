3205081235b8fd58ffc4ed98f9afc45d923df6be
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-12-13 13:50:14 -0500

    Get text from pitch and degree textbox.

--- Paranormal/Paranormal/ColorPickerViewController.swift
+++ Paranormal/Paranormal/ColorPickerViewController.swift
@@ -4,4 +4,38 @@ import AppKit
 
 class ColorPickerViewController: NSViewController {
     var document: Document?
+//    var phi: CGFloat = 0.0
+//    var the: CGFloat = 0.0
+    var x: Float32 = 0.0
+    var y: Float32 = 0.0
+    var z: Float32 = 0.0
+    var deg: Float32 = 0
+    var pit: Float32 = 0
+    
+    @IBOutlet weak var DegreeText: NSTextField!
+
+    @IBOutlet weak var PitchText: NSTextField!
+
+
+    @IBAction func getDegree(sender: AnyObject) {
+        deg = DegreeText.floatValue
+        x = sin(deg)*cos(pit)
+        y = sin(deg)*sin(pit)
+        z = cos(deg)
+    }
+
+    @IBAction func getPitch(sender: AnyObject) {
+        pit = PitchText.floatValue
+        x = sin(deg)*cos(pit)
+        y = sin(deg)*sin(pit)
+        z = cos(deg)
+        println(pit)
+    }
+
+    func calcColor(){
+        x = sin(1)
+        println(x)
+    }
+
 }
+
