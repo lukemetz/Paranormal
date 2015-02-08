0923c05c00954aefbb2abf2517b76d82af8ca1aa
Luke Metz <luke.metz@students.olin.edu>
2015-02-08 18:22:10 -0500

    Run GPU image on one thread and don't recreate it.

--- Paranormal/ParanormalTests/ThreadUtilsTest.swift
+++ Paranormal/ParanormalTests/ThreadUtilsTest.swift
@@ -16,6 +16,26 @@ class ThreadUtilsTest : QuickSpec {
                     ThreadUtils.runGPUImage(testFunc)
                     expect(toBeTrue).toEventually(beTrue())
                 }
+
+                it("Runs the block on a consistent thread") {
+                    var thread1 : NSThread?;
+                    var thread2 : NSThread?;
+
+                    func test1() {
+                        thread1 = NSThread.currentThread()
+                    }
+
+                    func test2() {
+                        thread2 = NSThread.currentThread()
+                    }
+
+                    ThreadUtils.runGPUImageDestructive(test1)
+                    ThreadUtils.runGPUImageDestructive(test2)
+
+                    expect(thread1).toEventuallyNot(beNil())
+                    expect(thread2).toEventuallyNot(beNil())
+                    expect(thread1).toEventually(equal(thread2))
+                }
             }
         }
     }
