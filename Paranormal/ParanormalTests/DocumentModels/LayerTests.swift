0885e7d433559d32197561aa6fb9416ced0e074d
Luke Metz <luke.metz@students.olin.edu>
2015-02-11 14:27:13 -0500

    Layer combinations now works correctly

--- Paranormal/ParanormalTests/DocumentModels/LayerTests.swift
+++ Paranormal/ParanormalTests/DocumentModels/LayerTests.swift
@@ -96,6 +96,13 @@ class LayerTests: QuickSpec {
                 expect(dummy.layers.count).to(equal(1))
                 expect(layer?.parent).to(equal(dummy))
             }
+
+            it("The new layer has image data") {
+                let dummy = dummyLayer()
+                let layer = dummy.addLayer()
+                expect(layer?.imageData).toNot(beNil())
+                expect(layer?.toImage()?.size).to(equal(NSSize(width:5, height:5)))
+            }
         }
 
         describe("removeLayer") {
@@ -112,8 +119,8 @@ class LayerTests: QuickSpec {
             }
         }
 
-        describe ("createEditLayer") {
-            it ("Should create a layer above the current layer") {
+        describe("createEditLayer") {
+            it("Should create a layer above the current layer") {
                 let dummy = dummyLayer()
                 let layer = dummy.addLayer()
                 let layer_after = dummy.addLayer()
@@ -121,6 +128,52 @@ class LayerTests: QuickSpec {
                 let editLayer = layer?.createEditLayer()
                 expect(dummy.layers.count).to(equal(3))
                 expect(dummy.layers.indexOfObject(editLayer!)).to(equal(1))
+            }
+
+            it("Should have some image data of the right size") {
+                let dummy = dummyLayer()
+                let layer = dummy.addLayer()
+                let edit = layer!.createEditLayer()!
+
+                expect(edit.imageData).toNot(beNil())
+                let image = edit.toImage()
+                expect(image).toNot(beNil())
+                expect(image!.size).to(equal(layer?.toImage()?.size))
+            }
+        }
+
+        describe("renderLayer") {
+            it("smoke test multi layers") {
+                var ran = false;
+                ThreadUtils.runGPUImage({ () -> Void in
+                    let dummy = dummyLayer()
+                    let layer1 = dummy.addLayer()
+                    let layer2 = dummy.addLayer()
+                    let layer3 = dummy.addLayer()
+                    let edit = layer1!.createEditLayer()!
+
+                    let render = dummy.renderLayer()
+                    expect(render).toNot(beNil())
+                    expect(render!.size.width).toNot(equal(0))
+                    expect(render!.size.height).toNot(equal(0))
+                    ran = true
+                })
+                expect(ran).toEventually(beTrue())
+            }
+
+            it("smoke 1 layer") {
+                var ran = false;
+                ThreadUtils.runGPUImage({ () -> Void in
+                    let dummy = dummyLayer()
+                    let layer = dummy.addLayer()
+
+                    let render = dummy.renderLayer()
+                    expect(render).toNot(beNil())
+                    expect(render!.size.width).toNot(equal(0))
+                    expect(render!.size.height).toNot(equal(0))
+                    ran = true
+                })
+                expect(ran).toEventually(beTrue())
             }
         }
     }
