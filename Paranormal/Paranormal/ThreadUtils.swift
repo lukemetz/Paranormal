1768d241b84d28ea626cedc019eba0dc0150e7e3
Luke Metz <luke.metz@students.olin.edu>
2015-03-26 12:14:38 -0700

    Don't use 300% of our cpu

--- Paranormal/Paranormal/ThreadUtils.swift
+++ Paranormal/Paranormal/ThreadUtils.swift
@@ -4,14 +4,21 @@ import GPUImage
 
 private let _ThreadUtilsShared = ThreadUtils()
 
+enum ThreadState : Int {
+    case Waiting = 0
+    case WorkTodo
+}
+
 public class ThreadUtils : NSObject {
     var gpuImageThread : NSThread?
     var editClosures : [() -> Void] = []
     var updateClosures : [() -> Void] = []
     var doneProcessing : Bool = true
     var lock : NSLock = NSLock()
+    var conditionLock : NSConditionLock
 
     override init() {
+        conditionLock = NSConditionLock(condition: ThreadState.Waiting.rawValue)
         super.init()
         gpuImageThread = NSThread(target: self,
             selector: NSSelectorFromString("threadMain"), object: nil)
@@ -20,6 +27,8 @@ public class ThreadUtils : NSObject {
 
     dynamic func threadMain() {
         while(true) { // TODO turn this off
+            conditionLock.lockWhenCondition(ThreadState.WorkTodo.rawValue)
+            conditionLock.unlockWithCondition(ThreadState.Waiting.rawValue)
             while (editClosures.count > 0) {
                 doneProcessing = false
                 lock.lock()
@@ -44,11 +53,18 @@ public class ThreadUtils : NSObject {
         return _ThreadUtilsShared
     }
 
+    private class func runWork() {
+        sharedInstance.conditionLock.lock()
+        sharedInstance.conditionLock.unlockWithCondition(ThreadState.WorkTodo.rawValue)
+    }
+
     public class func runGPUImage(block : () -> Void) {
         ThreadUtils.sharedInstance.doneProcessing = false
         sharedInstance.lock.lock()
         ThreadUtils.sharedInstance.updateClosures.append(block)
         sharedInstance.lock.unlock()
+        runWork()
+
     }
 
     public class func runGPUImageDestructive(block : () -> Void) {
@@ -56,6 +72,7 @@ public class ThreadUtils : NSObject {
         sharedInstance.lock.lock()
         ThreadUtils.sharedInstance.editClosures.append(block)
         sharedInstance.lock.unlock()
+        runWork()
     }
 
     class func runCocos(closure : () -> Void) {
